dcc2137ce4bc3df99f2208596ffef21d7e5feaa4
J_Kiser <jdkiser228@gmail.com>
2021-05-19 21:19:54 -0400

    creating workoutSchema

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Please enter an exercise type."
+        },
+        weight: {type: Number},
+        sets: {type: Number},
+        reps: {type:Number},
+        distance: {type:Number},
+        duration: {type:Number}
+    }]   
+}, {
+    toJSON: {
+        virtuals: true
+    }
+})
