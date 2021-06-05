d022e65ea793e0e7b6236e1841c6a0300d567e8a
J_Kiser <jdkiser228@gmail.com>
2021-06-05 13:31:45 -0400

    adding api info from tutor sesh

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,3 +17,39 @@ router.get("/api/workouts", (req, res) => {
         res.json(err);
     });
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+
+    db.Workout.findOneAndUpdate(
+        {_id: req.params.id},
+        {
+            $inc: { totalDuration: req.body.duration},
+            $push: {exercises: req.body}
+        },
+        { new:true}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+router.post("/api/workouts", ({body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        console.log("UP IN THE GYM");
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
